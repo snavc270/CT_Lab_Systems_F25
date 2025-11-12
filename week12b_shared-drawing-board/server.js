6d5a8c737ab325d82a6dd9ef6a94d91ac2186334
Courtney Snavely <courtneysnavely@Courtneys-MBP.lan>
2025-11-11 20:48:16 -0500

    added shared canvas drawing example

---
+++ week12b_shared-drawing-board/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const PORT = 3000;
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("🟢 New user connected:", socket.id);
+
+  socket.on("draw", (data) => {
+    // rebroadcast to everyone except the sender
+    socket.broadcast.emit("draw", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔴 User disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${3000}`));
+
